85d085a6653cda976d8faca1a72355b81b73e115
Alya Ghaitsaa <58002736+aliyahuyy@users.noreply.github.com>
2021-11-01 08:58:27 +0700

    Add files via upload

---
+++ UDPHeader.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jarkom.week.pkg5;
+import java.util.Scanner;
+
+/**
+ *
+ * @author alyaghaitsa
+ */
+public class UDPHeader {
+     public static void main(String[] args) {  
+          
+        Scanner input = new Scanner(System.in);  
+ 
+        System.out.print("Input UDP Header : ");            
+        String UDPHeader = input.nextLine();
+        System.out.println(" ");
+
+        
+        System.out.println("1. SOURCE PORT NUMBER: Hexadesimal "+ UDPHeader.substring(0, 4) + " atau Desimal " + konversiHexadecimal(UDPHeader.substring(0, 4)));
+        System.out.println("2. DESTINATION PORT NUMBER: Hexadesimal "+ UDPHeader.substring(4, 8) + " atau Desimal " + konversiHexadecimal(UDPHeader.substring(4, 8)));
+        System.out.println("3. TOTAL LENGHT OF THE USER DATAGRAM: Hexadesimal "+ UDPHeader.substring(8,12)+" atau total panjang UDP paket adalah " + konversiHexadecimal(UDPHeader.substring(8, 12)) +" byte");
+     }
+     public static int konversiHexadecimal(String hex) {  
+          
+        int nilaiDesimal=0;  
+          
+        for (int i = 0; i < hex.length(); i++) {   
+             char hexChar = hex.charAt(i);  
+             nilaiDesimal = nilaiDesimal * 16 + hexCharKeDesimal(hexChar);  
+             }  
+        return nilaiDesimal;  
+          
+   }  
+      
+   public static int hexCharKeDesimal(char ch) {  
+        if (ch >= 'A' && ch <= 'F')  
+             return 10 + ch - 'A';  
+        else 
+             return ch - '0';  
+        }  
+}
+     
+
+
